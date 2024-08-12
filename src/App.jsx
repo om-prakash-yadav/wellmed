2fad12852439a316fa6af4d83a61065f6f87dfe9
Abhishek <abhi1nimsurya@gmail.com>
2024-08-12 17:33:07 +0530

    this is abhishek's commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/Navbar'
+import Service from './components/Service'
 
 const App = () => {
   return (
    <div>
     <NavBar/>
+    <Service/>
    </div>
   )
 }
