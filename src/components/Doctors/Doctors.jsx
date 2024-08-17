7294c7d2f4b538d5d7eea3c485c4369355a3ab52
Mohit6204 <mishramohit6204@gmail.com>
2024-08-17 15:49:24 +0530

    doctors page completed

--- src/components/Doctors/Doctors.jsx
+++ src/components/Doctors/Doctors.jsx
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import './Doctors.css'
-import { IoLocationSharp } from "react-icons/io5";
+import DoctorCard from './DoctorCard';
 
 const Doctors = () => {
   const [data,setData]=useState([]);
-  const [date,setDate]=useState('');
   useEffect(()=>{
     axios.get(' http://localhost:3000/Doctors')
     .then((value)=>{
       setData(value.data);
     })
-  },[])
+  },[]);
   return (
     <div>
      <div className='header'>
@@ -20,34 +19,7 @@ const Doctors = () => {
      <div className='container'>
          {data.length&&data.map((item,index)=>{
             return (
-              <div className='card'>
-                <div className='card_header'>
-                  <img src={item.img_src} alt="" />
-                </div>
-                <div className='main'>
-                <div className='card-content'>
-                   <h3>{item.name}</h3>
-                   <div className='card-main'>
-                   <h5>{item.specialization}</h5>
-                   <div className='location-bar'>
-                      <div className='location-icon'>
-                      <IoLocationSharp />
-                      </div>
-                      <div className='location'>
-                         {item.location}
-                      </div>
-                  </div>
-                   </div>
-                   <p>{item.about}</p>
-                </div>
-                <div className='card-footer'>
-                  <input type="date" value={date} />
-                  <div>
-                     <button className='appointment'>Appointment</button>
-                  </div>
-                </div>
-                </div>
-              </div>
+               <DoctorCard item={item} key={index}/>
             )
          })}
      </div>
