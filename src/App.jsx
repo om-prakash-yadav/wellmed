aa17d2e2d669bc0d6c00799257606bdcd24fcd7b
om-prakash-yadav <jnvomprakash121@gmail.com>
2024-08-18 14:03:37 +0530

    navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Doctors from './components/Doctors/Doctors';
 import Support from './components/Support/Support';
 import MediStore from './components/Medistore/MediStore'
 import Design from './components/Health_blog/health_blog';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './NavBar.css';
 import { useNavigate } from 'react-router-dom';
-
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css';
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -10,25 +10,41 @@ const NavBar = () => {
   const handleImageClick = () => {
     navigate('/');
   };
+
   return (
-    <header className="navbar">
-      <div className="logo">
-        <img src="logo.png" onClick={handleImageClick} alt="" />
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <div className="navbar-brand" onClick={handleImageClick}>
+          <img src="logo.png" alt="Logo" height="50" />
+        </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/doctors">Doctors</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/store">MediStore</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/support">Support</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/blogs">Health Blogs</Link>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center start-container">
+            <button className="btn btn-outline-primary me-2 get-started ">GET STARTED</button>
+            <div className='back-div'></div>
+          </div>
+        </div>
       </div>
-      <nav>
-        <ul className="nav-links menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/doctors">Doctors</Link></li>
-          <li><Link to="/store">MediStore</Link></li>
-          <li><Link to="/support">Support</Link></li>
-          <li><Link to="/blogs">Health Blogs</Link></li>
-        </ul>
-      </nav>
-      <div class="container">
-        <div class="get-started">GET STARTED</div>
-        <div className='back-div'></div>
-      </div>
-    </header>
+    </nav>
   );
 };
 
