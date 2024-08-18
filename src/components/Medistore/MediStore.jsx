899c5327a01082f293648fde2be58c6ed1ebc4fc
om-prakash-yadav <jnvomprakash121@gmail.com>
2024-08-18 20:40:33 +0530

    medicines added

--- src/components/Medistore/MediStore.jsx
+++ src/components/Medistore/MediStore.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import './Medistore.css'
 
 const MediStore = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/medicines')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
   return (
-    <div>MediStore</div>
-  )
+    <div className="product-container">
+      {products.map(product => (
+        <div className="product-card" key={product.id}>
+          <div className="product-image">
+            <img src={product.image} alt={product.alt} />
+          </div>
+          <div className="product-details">
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <div className="price-add">
+              <span className="price">${product.price}</span>
+              <button className="add-to-cart">
+                <span><i className="fa fa-shopping-cart"></i></span> Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default MediStore
+
+export default MediStore;
