8cc3d4b6fa31cf4d12bff85293dc74c60a9c5cc2
om-prakash-yadav <jnvomprakash121@gmail.com>
2024-08-19 11:25:08 +0530

    filters added

--- src/components/Medistore/MediStore.jsx
+++ src/components/Medistore/MediStore.jsx
@@ -1,14 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from "axios";
-import './Medistore.css'
+import './Medistore.css';
 import { CartContext } from '../../contexts/CartContext';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaSearch, FaSortAmountDown, FaSortAmountUp } from 'react-icons/fa';
 
 const MediStore = () => {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
   const { cartItems, addToCart } = useContext(CartContext);
   const notify = () => toast.success(`Product added to cart`);
+
   useEffect(() => {
     axios.get('http://localhost:3000/medicines')
       .then(response => {
@@ -25,20 +29,44 @@ const MediStore = () => {
       await axios.post(`http://localhost:3000/cartitems`, JSON.stringify(product));
       addToCart(product);
       notify();
-    }
-    else {
+    } else {
       toast.error("Item already in your cart");
     }
-
   }
+
+  const filteredProducts = products
+    .filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    .sort((a, b) => {
+      if (sortOrder === 'lowToHigh') {
+        return a.price - b.price;
+      } else if (sortOrder === 'highToLow') {
+        return b.price - a.price;
+      } else {
+        return 0;
+      }
+    });
 
   return (
     <div className="container-fluid">
-      <h1 className='text-center bg-white mt-6 p-6'>Our Medicines</h1>
+      <h1 style={{marginTop:'30px'}} className='text-center bg-white '>Our Medicines</h1>
+
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Search medicines..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+        <select onChange={(e) => setSortOrder(e.target.value)} className="sort-select">
+          <option value="">Sort by</option>
+          <option value="lowToHigh">Price: Low to High</option>
+          <option value="highToLow">Price: High to Low</option>
+        </select>
+      </div>
 
       <div className="product-container">
-
-        {products.map(product => (
+        {filteredProducts.map(product => (
           <div className="product-card" key={product.id}>
             <div className="product-image">
               <img src={product.image} alt={product.alt} />
@@ -60,6 +88,5 @@ const MediStore = () => {
     </div>
   );
 }
-
 
 export default MediStore;
